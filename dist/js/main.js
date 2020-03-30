ce0f3d1f728b5838b0a4b9fa75590aa3f262474b
zempo <zelenkosolomon@gmail.com>
2020-03-29 22:54:10 -0700

    fix router bug

--- dist/js/main.js
+++ dist/js/main.js
@@ -49,12 +49,36 @@ const runCarousel = () => {
       new TextCarousel(elements[i], JSON.parse(toRotate), duration);
     }
   }
+  console.log(elements);
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
   css.innerHTML = ".txt-carousel > .wrap { border-right: 0.08em solid #000 }";
   document.body.appendChild(css);
 };
+
+window.addEventListener("load", e => {
+  const hashLocation = window.location.hash.substr(1);
+  const slot = document.getElementById("container");
+  if (!window.location.hash) {
+    fetch("routes/home.html")
+      // to-do: event is also firing as a hashchange, make sure to account for this
+      .then(r => r.text())
+      .then(content => {
+        slot.innerHTML = content;
+      });
+  }
+  if (hashLocation.includes("home")) {
+    runCarousel();
+  }
+});
+
+window.addEventListener("hashchange", e => {
+  const hashLocation = window.location.hash.substr(1);
+  if (hashLocation.includes("home")) {
+    runCarousel();
+  }
+});
 
 class IndexView {
   constructor() {
@@ -65,16 +89,23 @@ class IndexView {
   onRouteChange(e) {
     const hashLocation = window.location.hash.substr(1);
     this.loadContent(hashLocation);
-    if (hashLocation.includes("home")) {
-      runCarousel();
-    }
   }
 
   loadContent(uri) {
     const contentURI = `routes/${uri}.html`;
-    fetch(contentURI)
-      .then(r => r.text())
-      .then(content => this.updateSlot(content));
+
+    if (contentURI === "routes/.html") {
+      fetch("routes/home.html")
+        // to-do: event is also firing as a hashchange, make sure to account for this
+        .then(r => r.text())
+        .then(content => {
+          slot.innerHTML = content;
+        });
+    } else {
+      fetch(contentURI)
+        .then(r => r.text())
+        .then(content => this.updateSlot(content));
+    }
   }
 
   updateSlot(content) {
