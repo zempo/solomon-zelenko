2edf5b598bed5679415afd448ae1fd102634d1b1
zempo <zelenkosolomon@gmail.com>
2020-04-24 19:18:03 -0700

    fix entity inconsistencies

--- dist/js/main.js
+++ dist/js/main.js
@@ -59,7 +59,15 @@ const handleMailForms = (hire, tutor, gen) => {
     if(resOk) {
       formStatus.classList.add('success-status')    
       formStatus.innerHTML = `
-      <span class="success">&#10004;</span>
+      <span class="success">
+      <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24">
+      <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/>
+      </svg>
+      </span>
       <div class="msg">
       <p>${msgTop}</p>
       <p>${msgBottom}</p>
@@ -68,7 +76,15 @@ const handleMailForms = (hire, tutor, gen) => {
     } else {
       formStatus.classList.add('err-status')
       formStatus.innerHTML = `
-      <span class="err">&#10005;</span>
+      <span class="err">
+      <svg
+       xmlns="http://www.w3.org/2000/svg"
+       width="24"
+       height="24"
+       viewBox="0 0 24 24">
+       <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/>
+       </svg>
+      </span>
       <div class="msg">
       <p>${msgTop}</p>
       <p>${msgBottom}</p>
@@ -76,7 +92,7 @@ const handleMailForms = (hire, tutor, gen) => {
       `
     }
   }
-  
+
   // submit handlers
   hire.addEventListener('submit', e => {
     e.preventDefault()
